6a3633280e88cf07400717e28a2f1e13fba98d64
Cormac Walsh <walshcormac1@gmail.com>
2022-03-19 13:56:03 -0700

    I hit a bug, think I need to push

---
+++ SpellChk.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class SpellChk{
+    public static void main(String[] args) throws Exception{
+        FileInputStream fil = new FileInputStream("C://Users//cormacwalsh//usa.txt");
+    }
+}
