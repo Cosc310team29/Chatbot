e75a9ce704ad9de0a8d5e025936bb4adb5b9380a
Cosc310team29 <100541933+Cosc310team29@users.noreply.github.com>
2022-03-18 20:21:40 -0700

    Update GUI.java
    small changes to handle input error

--- GUI.java
+++ GUI.java
@@ -23,19 +23,27 @@ public class GUI {
        JOptionPane.showMessageDialog(frame, "You wrote: " + textInput);
         System.out.println(textInput);
         if (textInput.equals("Hello")) {
+        	for (int j = 0; j <30; j++){
         	String option = JOptionPane.showInputDialog(frame, "what can I help you today?\r\n" + "1 issue with the item\r\n"
         			+ "2 item not yet received\r\n"
         			+ "3 comments to the item\r\n"
         			+ "4 for the person-person customer service\r\n");
         	if (option.equals("1")) {
         		JOptionPane.showMessageDialog(frame, " Your item have an issue");
+        		j=31;
         	}else if (option.equals("2")) {
         		JOptionPane.showMessageDialog(frame, " Your item will be arrive in serval days");
+        		j=31;
         }else if (option.equals("3")) {
     		JOptionPane.showMessageDialog(frame, " We have recieved your comments");
+    		j=31;
     }else if (option.equals("4")) {
 		JOptionPane.showMessageDialog(frame, " The number of customer service is (604)-617-7777");
+		j=31;
+}else {
+	JOptionPane.showMessageDialog(frame, "You wrote an invalid command, please try again ");
 }
+        }
         }else {
         	JOptionPane.showMessageDialog(frame, "You wrote an invalid command, please try again ");
         }
@@ -47,5 +55,3 @@ public class GUI {
         
     }
 
-
-}
