6885caff4492d39efcd7e78d29e3af1fe388f498
Cosc310team29 <100541933+Cosc310team29@users.noreply.github.com>
2022-03-18 20:15:55 -0700

    Update GUI.java
    added new response by Owen Wong

--- GUI.java
+++ GUI.java
@@ -1,3 +1,4 @@
+package gui;
 import java.awt.BorderLayout;
 
 import javax.swing.*;
@@ -18,14 +19,32 @@ public class GUI {
         frame.pack();
         frame.setVisible(true);
         for (int i = 0; i <30; i++){
-        String textInput = JOptionPane.showInputDialog(frame, "Enter text to chat: ");
+        String textInput = JOptionPane.showInputDialog(frame, "Enter Hello to chat: ");
        JOptionPane.showMessageDialog(frame, "You wrote: " + textInput);
         System.out.println(textInput);
+        if (textInput.equals("Hello")) {
+        	String option = JOptionPane.showInputDialog(frame, "what can I help you today?\r\n" + "1 issue with the item\r\n"
+        			+ "2 item not yet received\r\n"
+        			+ "3 comments to the item\r\n"
+        			+ "4 for the person-person customer service\r\n");
+        	if (option.equals("1")) {
+        		JOptionPane.showMessageDialog(frame, " Your item have an issue");
+        	}else if (option.equals("2")) {
+        		JOptionPane.showMessageDialog(frame, " Your item will be arrive in serval days");
+        }else if (option.equals("3")) {
+    		JOptionPane.showMessageDialog(frame, " We have recieved your comments");
+    }else if (option.equals("4")) {
+		JOptionPane.showMessageDialog(frame, " The number of customer service is (604)-617-7777");
+}
+        }else {
+        	JOptionPane.showMessageDialog(frame, "You wrote an invalid command, please try again ");
         }
+    }
     }
     public static void main(String[] args){
    
         new GUI();
+        
     }
 
 
