a020c68f8a01abbcc26a7614ca808f7bbfd2db72
Cormac Walsh <walshcormac1@gmail.com>
2022-03-19 20:34:49 -0700

    added more logic to converse

--- GUI.java
+++ GUI.java
@@ -3,6 +3,9 @@ import java.awt.BorderLayout;
 import java.util.Dictionary;
 import java.awt.*;
 import javax.swing.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 
 public class GUI {
 
@@ -12,7 +15,8 @@ public class GUI {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         JLabel label = new JLabel("Click anywhere to start conversation");
-  
+        String textInput = "";
+
         panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 300, 300));
         panel.add(label);
         frame.add(panel, BorderLayout.CENTER);
@@ -21,25 +25,29 @@ public class GUI {
         frame.pack();
         frame.setVisible(true);
         for (int i = 0; i <30; i++){
-        String textInput = JOptionPane.showInputDialog(frame, "Enter Hello to chat: ");
-        JOptionPane.showMessageDialog(frame, "You wrote: " + textInput);
-        System.out.println(textInput);
+        textInput = JOptionPane.showInputDialog(frame, "Enter Hello to chat: ");
 
-        if (textInput.equals("Hello")) {
+        if (textInput.equalsIgnoreCase("Hello") || textInput.equalsIgnoreCase("Hey") || textInput.equalsIgnoreCase("Hi")) {
         	for (int j = 0; j <30; j++){
         	String option = JOptionPane.showInputDialog(frame, "what can I help you today?\r\n" + "1 issue with the item\r\n"
         			+ "2 item not yet received\r\n"
         			+ "3 comments to the item\r\n"
         			+ "4 for the person-person customer service\r\n");
         	if (option.equals("1")) {
-        		JOptionPane.showMessageDialog(frame, " Your item have an issue");
-        		j=31;
+                JOptionPane.showMessageDialog(frame, " Your item have an issue");
+                j = 31;
         	}else if (option.equals("2")) {
-        		JOptionPane.showMessageDialog(frame, " Your item will be arrive in serval days");
-        		j=31;
-        }else if (option.equals("3")) {
-    		JOptionPane.showMessageDialog(frame, " We have recieved your comments");
-    		j=31;
+                JOptionPane.showMessageDialog(frame, " Your item will be arrive in serval days");
+                j = 31;
+            }else if (option.equals("3")) {
+            JOptionPane.showMessageDialog(frame, " We have recieved your comments");
+            
+            if (textInput.equalsIgnoreCase("What time is it") || textInput.equalsIgnoreCase("What day is it")){
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                LocalDateTime current = LocalDateTime.now();
+                JOptionPane.showMessageDialog(frame, dtf.format(current));
+                }
+                j = 31;
     }else if (option.equals("4")) {
 		JOptionPane.showMessageDialog(frame, " The number of customer service is (604)-617-7777");
 		j=31;
@@ -51,7 +59,12 @@ public class GUI {
         	JOptionPane.showMessageDialog(frame, "You wrote an invalid command, please try again ");
         }
     }
-    }
+    
+
+    JOptionPane.showMessageDialog(frame, "You wrote: " + reply);
+
+
+}
 
     public static void main(String[] args){
    
